6285ce3fb902a4c2e5e4559ab17579af13985393
Kim Hicks <hicksmomcodes@gmail.com>
2020-12-15 00:32:00 -0500

    Changes to index.html, styles.css, and scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,61 @@
 // ---------- BEGIN BASIC PAGE ----------
 
+// const navBurger = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.main-nav');
 
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('.main-nav-active')
+//     });
+// }
 
+// navSlide();
 
 // ---------- END BASIC PAGE ----------
+
+// ---------- CAROUSEL FLOW ----------
+
+const carouselSlide = document.querySelector('.carousel-slide')
+const carouselImage = document.querySelectorAll('.carousel-slide img');
+
+// Buttons
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+// Image Counter
+let counter = 1;
+const size = carouselImage[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button Listeners
+
+nextButton.addEventListener('click',() => {
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';    
+});
+
+prevButton.addEventListener('click',()=>{
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';    
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';    
+    };
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';    
+    };
+});
+
+
+
